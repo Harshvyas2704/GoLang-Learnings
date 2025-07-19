780a611375b46fd7a25f87643926b869d55d7c21
Harshvyas2704 <harshvyasdev@gmail.com>
2025-07-19 22:24:20 +0530

    Booking app init

---
+++ Booking-App/main.go
@@ -0,0 +1,11 @@
+package main
+
+import "fmt" // formate package
+
+func main() {
+	var conferenceName = "Go Conference"
+	fmt.Println(conferenceName)
+	fmt.Println("Welcome to our conference booking application")
+	fmt.Println("Thank you for booking in our conference")
+
+}
